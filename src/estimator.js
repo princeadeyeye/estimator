32f4e562b5625f305d0773c7b6e8ade9c2a870ef
teezyfortune <muizadeyeye@gmail.com>
2020-04-09 14:00:14 +0100

    assessment 2

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
 const impactCovid = (data) => {
-  const { reportedCases, totalHospitalBeds } = data;
+  const { reportedCases, totalHospitalBeds, periodType } = data;
   const impact = {};
   let {
     currentlyInfected,
@@ -13,7 +13,7 @@ const impactCovid = (data) => {
     dollarsInFlight
   } = impact;
   currentlyInfected = reportedCases * 10;
-  infectionsByRequestedTime = currentlyInfected * 1024;
+  infectionsByRequestedTime = periodType === 'months' ? currentlyInfected * 1024 : currentlyInfected * 512;
   severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
   severeAvBeds = 0.35 * totalHospitalBeds;
   remainbeds = severeCasesByRequestedTime - severeAvBeds;
