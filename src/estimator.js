d2a2b8e9862e5e941beaa5da072c1838e8ea7b22
teezyfortune <muizadeyeye@gmail.com>
2020-04-10 02:55:35 +0100

    assesment xxx

--- src/estimator.js
+++ src/estimator.js
@@ -15,7 +15,8 @@ const impactCovid = (data) => {
     reportedCases,
     totalHospitalBeds,
     periodType,
-    timeToElapse
+    timeToElapse,
+    region: { avgDailyInUSD, avgDailyIncomePopulation }
   } = data;
   const impact = {};
   let {
@@ -39,9 +40,12 @@ const impactCovid = (data) => {
   remainbeds = Math.trunc(severeAvBeds - severeCasesByRequestedTime);
   hospitalBedsByRequestedTime = (severeAvBeds > severeCasesByRequestedTime)
     ? severeAvBeds : remainbeds;
-  casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
-  casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
-  dollarsInFlight = infectionsByRequestedTime * 1 * 1.5 * 30;
+  casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
+  casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
+  const dollars = Number(
+    infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyInUSD * 30
+  ).toFixed(2);
+  dollarsInFlight = Math.trunc(dollars);
   return {
     currentlyInfected,
     infectionsByRequestedTime,
@@ -60,7 +64,8 @@ const severeCovid = (data) => {
     reportedCases,
     totalHospitalBeds,
     periodType,
-    timeToElapse
+    timeToElapse,
+    region: { avgDailyInUSD, avgDailyIncomePopulation }
   } = data;
   const severeImpact = {};
   let {
@@ -84,9 +89,11 @@ const severeCovid = (data) => {
   remainbeds = Math.trunc(severeAvBeds - severeCasesByRequestedTime);
   hospitalBedsByRequestedTime = (severeAvBeds > severeCasesByRequestedTime)
     ? severeAvBeds : remainbeds;
-  casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
-  casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
-  dollarsInFlight = infectionsByRequestedTime * 1 * 1.5 * 30;
+  casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
+  casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
+  dollarsInFlight = Number(
+    infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyInUSD * 30
+  ).toFixed(2);
   return {
     currentlyInfected,
     infectionsByRequestedTime,
