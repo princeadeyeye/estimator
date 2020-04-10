35ecf64c1c4b0a90ed9c733fd5f4491118295f2c
teezyfortune <muizadeyeye@gmail.com>
2020-04-10 16:03:28 +0100

    assessment x

--- src/estimator.js
+++ src/estimator.js
@@ -82,7 +82,8 @@ const severeCovid = (data) => {
   hospitalBedsByRequestedTime = Math.trunc(severeAvBeds - severeCasesByRequestedTime);
   casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
   casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
-  dollarsInFlight = infectionsByRequestedByDays * avgDailyIncomePopulation * avgDailyIncomeInUSD * 30;
+  dollarsInFlight = infectionsByRequestedByDays * avgDailyIncomePopulation 
+  * avgDailyIncomeInUSD * 30;
   return {
     currentlyInfected,
     infectionsByRequestedTime,
